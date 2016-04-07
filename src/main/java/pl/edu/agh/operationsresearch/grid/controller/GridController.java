38b77e9e433b0930ff6bb47fd868c456aff06c90
Jakub Stępak <jakub.ste@gmail.com>
2016-04-08 00:19:28 +0200

    Change grid in GridController to 2D

--- src/main/java/pl/edu/agh/operationsresearch/grid/controller/GridController.java
+++ src/main/java/pl/edu/agh/operationsresearch/grid/controller/GridController.java
@@ -14,20 +14,23 @@ public class GridController implements Initializable {
     @FXML
     private GridPane sudokuGridPane;
 
-    private GridCell[] sudokuGrid;
+    private GridCell[][] sudokuGrid;
 
+    private static final int GRID_SIZE = 9;
 
     public void initialize(URL location, ResourceBundle resources) {
-        sudokuGrid = new GridCell[81];
+        sudokuGrid = new GridCell[GRID_SIZE][GRID_SIZE];
 
-        for(int i=0; i<81; i++) {
-            sudokuGrid[i] = new GridCell();
-            sudokuGridPane.add(sudokuGrid[i], i%9, i/9);
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                sudokuGrid[i][j] = new GridCell();
+                sudokuGridPane.add(sudokuGrid[i][j], i, j);
+            }
         }
     }
 
 
-    public GridCell[] getSudokuGrid() {
+    public GridCell[][] getSudokuGrid() {
         return sudokuGrid;
     }
 
