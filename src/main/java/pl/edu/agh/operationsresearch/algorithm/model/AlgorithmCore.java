96a49b4f70864c50caa10cd5d5b92159d5f4f374
bgrochal <bartek.grochal@gmail.com>
2016-04-18 18:47:53 +0200

    Working on algorithm. Pseudocode implemented fully.

--- src/main/java/pl/edu/agh/operationsresearch/algorithm/model/AlgorithmCore.java
+++ src/main/java/pl/edu/agh/operationsresearch/algorithm/model/AlgorithmCore.java
@@ -5,6 +5,7 @@ import pl.edu.agh.operationsresearch.grid.controller.GridController;
 import pl.edu.agh.operationsresearch.grid.model.GridCell;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class AlgorithmCore {
     private static final int SUB_GRIDS = 3;
@@ -15,11 +16,15 @@ public class AlgorithmCore {
 
     private double evaporationRate;
 
-    private int antsNumber;
     private int globalMaxSelected;
+    private int antsNumber;
+
+    private Random randomGenerator;
 
     // indicates pheromone value on position (i, j) with number k
     private double[][][] pheromoneValue;
+    private double[][][] probability;
+    private double[][][] weights;
 
     private int[][][] availableSubGridPlaces;
     private int[][] availableGridCellDigits;
@@ -42,10 +47,26 @@ public class AlgorithmCore {
         initializePheromones();
         initializeArrays();
 
+        randomGenerator = new Random();
         globalMaxSelected = 0;
     }
 
+    private void initializePheromones() {
+        pheromoneValue = new double[GRID_SIZE][GRID_SIZE][NUMBERS];
+
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                for (int k = 0; k < NUMBERS; k++) {
+                    pheromoneValue[i][j][k] = PHEROMONES_MAX;
+                }
+            }
+        }
+    }
+
     private void initializeArrays() {
+        probability = new double[GRID_SIZE][GRID_SIZE][NUMBERS];
+        weights = new double[GRID_SIZE][GRID_SIZE][NUMBERS];
+
         availableSubGridPlaces = new int[SUB_GRIDS][SUB_GRIDS][NUMBERS];
         availableGridCellDigits = new int[GRID_SIZE][GRID_SIZE];
 
@@ -68,6 +89,7 @@ public class AlgorithmCore {
 
     private void cycleLoop() {
         GridCell[][] resultMatrix = GridController.getInstance().getSudokuGrid();
+        GridCell[][] currentlyBestResult = new GridCell[GRID_SIZE][GRID_SIZE];
         GridCell[][] workMatrix = new GridCell[GRID_SIZE][GRID_SIZE];
 
         int currentlySelected;
@@ -98,18 +120,65 @@ public class AlgorithmCore {
                     }
 
                     currentlySelected = manageWholeGrid(currentlySelected, workMatrix);
+                    double weightsSum = calculateWeights();
+                    calculateProbability(weightsSum);
+
+                    // TODO: Not sure if it is correct.
+                    double p = randomGenerator.nextDouble();
+                    for(int i=0; i<GRID_SIZE; i++) {
+                        for(int j=0; j<GRID_SIZE; j++) {
+                            double probabilitiesSum = 0;
+
+                            for(int k=1; k<NUMBERS; k++) {
+                                probabilitiesSum += probability[i][j][k];
+                                if(probabilitiesSum > p) {
+                                    workMatrix[i][j].setValue(k);
+                                    currentlySelected++;
+
+                                    // TODO: Should we now check, whether this selection hasn't caused a conflict with previously selected items?
+
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    if(currentlySelected == 81) {
+                        canSelect = false;
+                    }
+                }
+
+                if(currentlySelected > maxSelected) {
+                    maxSelected = currentlySelected;
+                    rewriteSudokuGrid(workMatrix, currentlyBestResult);
                 }
             }
-        }
-    }
 
-    private void initializePheromones() {
-        pheromoneValue = new double[GRID_SIZE][GRID_SIZE][NUMBERS];
+            double pheromoneLeft = maxSelected/81;
 
-        for (int i = 0; i < GRID_SIZE; i++) {
-            for (int j = 0; j < GRID_SIZE; j++) {
-                for (int k = 0; k < NUMBERS; k++) {
-                    pheromoneValue[i][j][k] = PHEROMONES_MAX;
+            for(int i=0; i<GRID_SIZE; i++) {
+                for(int j=0; j<GRID_SIZE; j++) {
+                    for(int k=1; k<NUMBERS; k++) {
+                        pheromoneValue[i][j][k] *= evaporationRate;
+                    }
+                }
+            }
+
+            for(int i=0; i<GRID_SIZE; i++) {
+                for(int j=0; j<GRID_SIZE; j++) {
+                    if(currentlyBestResult[i][j].getValue() != 0) {
+                        pheromoneValue[i][j][currentlyBestResult[i][j].getValue()] += pheromoneLeft;
+                    }
+                }
+            }
+
+            if(maxSelected > globalMaxSelected) {
+                globalMaxSelected = maxSelected;
+
+                for(int i=0; i<GRID_SIZE; i++) {
+                    for(int j=0; j<GRID_SIZE; j++) {
+                        resultMatrix[i][j].setValue(currentlyBestResult[i][j].getValue());
+                    }
                 }
             }
         }
@@ -209,6 +278,7 @@ public class AlgorithmCore {
         return selected;
     }
 
+    // TODO: Shouldn't this method declare exception as manageSubGrids does?
     private int manageWholeGrid(int selected, GridCell[][] matrix) {
         for(int i=0; i<GRID_SIZE; i++) {
             for(int j=0; j<GRID_SIZE; j++) {
@@ -246,6 +316,31 @@ public class AlgorithmCore {
         }
         
         return selected;
+    }
+
+    private double calculateWeights() {
+        double weightsSum = 0;
+
+        for(int i=0; i<GRID_SIZE; i++) {
+            for(int j=0; j<GRID_SIZE; j++) {
+                for(int k=1; k<NUMBERS; k++) {
+                    weights[i][j][k] = pheromoneValue[i][j][k] * (10 - availableSubGridPlaces[i][j][k]) * (10 - availableGridCellDigits[i][j]);
+                    weightsSum += weights[i][j][k];
+                }
+            }
+        }
+
+        return weightsSum;
+    }
+
+    private void calculateProbability(double weightsSum) {
+        for(int i=0; i<GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                for (int k = 1; k < NUMBERS; k++) {
+                    probability[i][j][k] = weights[i][j][k] / weightsSum;
+                }
+            }
+        }
     }
 
     private boolean isDigitSelectedInRow(int row, int digit, GridCell[][] matrix) {
